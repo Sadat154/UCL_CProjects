8f5f3f99953617123b3d92f781f12b1cfb18266a
Sadat154 <nafissadat6@gmail.com>
2024-10-02 13:45:01 +0100

    Starting on Exercises

---
+++ House/Octagon.c
@@ -0,0 +1,9 @@
+#include "graphics.h"
+
+
+int main(void)
+{
+    drawLine(140,50, 190,50);
+    return 0;
+}
+//use 8-iterations for loop and rotation matrix
---
+++ House/main.c
@@ -0,0 +1,34 @@
+// #include "graphics.h"
+// int main(void)
+// {
+//  drawRect(50,100,200,150);
+//  drawLine(50,100,150,25);
+//  drawLine(150,25,250,100);
+//  drawRect(130,190,40,60);
+//  drawRect(70,195,40,30);
+//  drawRect(190,195,40,30);
+//  drawRect(70,125,40,30);
+//  drawRect(190,125,40,30);
+//  return 0;
+// }
+
+
+#include "graphics.h"
+// int main(void)
+// {
+//  drawLine(0,0,499,299);
+//  setColour(green);
+//  fillRect(150,50,100,200);
+//  return 0;
+// }
+
+int main(void)
+{
+    // drawArc(50,90,150,150,0,90);
+    // drawOval(0,0,400,200);
+    // setColour(green);
+    // setRGBColour(255,0,0);
+    setWindowSize(2500,1500);
+    drawString("Hello", 250, 100);
+    return 0;
+}
